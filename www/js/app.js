3eeec5ed49a09e1b08c9fe2e51b27cee16c08c39
Raffaele Candeliere <raffaele.candeliere@gmail.com>
2016-04-20 15:21:54 +0200

    Aggiunto il codice di verifica transazioni in sospeso, ma ci sono punti da verificare

--- www/js/app.js
+++ www/js/app.js
@@ -2,6 +2,77 @@
 
 var Buffer = buffer.Buffer;
 
+function htmlspecialchars (string, quote_style, charset, double_encode) {
+  //       discuss at: http://phpjs.org/functions/htmlspecialchars/
+  //      original by: Mirek Slugen
+  //      improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+  //      bugfixed by: Nathan
+  //      bugfixed by: Arno
+  //      bugfixed by: Brett Zamir (http://brett-zamir.me)
+  //      bugfixed by: Brett Zamir (http://brett-zamir.me)
+  //       revised by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+  //         input by: Ratheous
+  //         input by: Mailfaker (http://www.weedem.fr/)
+  //         input by: felix
+  // reimplemented by: Brett Zamir (http://brett-zamir.me)
+  //             note: charset argument not supported
+  //        example 1: htmlspecialchars("<a href='test'>Test</a>", 'ENT_QUOTES');
+  //        returns 1: '&lt;a href=&#039;test&#039;&gt;Test&lt;/a&gt;'
+  //        example 2: htmlspecialchars("ab\"c'd", ['ENT_NOQUOTES', 'ENT_QUOTES']);
+  //        returns 2: 'ab"c&#039;d'
+  //        example 3: htmlspecialchars('my "&entity;" is still here', null, null, false);
+  //        returns 3: 'my &quot;&entity;&quot; is still here'
+
+  var optTemp = 0,
+    i = 0,
+    noquotes = false
+  if (typeof quote_style === 'undefined' || quote_style === null) {
+    quote_style = 2
+  }
+  string = string || ''
+  string = string.toString()
+  if (double_encode !== false) {
+    // Put this first to avoid double-encoding
+    string = string.replace(/&/g, '&amp;')
+  }
+  string = string.replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+
+  var OPTS = {
+    'ENT_NOQUOTES': 0,
+    'ENT_HTML_QUOTE_SINGLE': 1,
+    'ENT_HTML_QUOTE_DOUBLE': 2,
+    'ENT_COMPAT': 2,
+    'ENT_QUOTES': 3,
+    'ENT_IGNORE': 4
+  }
+  if (quote_style === 0) {
+    noquotes = true
+  }
+  if (typeof quote_style !== 'number') {
+    // Allow for a single string or an array of string flags
+    quote_style = [].concat(quote_style)
+    for (i = 0; i < quote_style.length; i++) {
+      // Resolve string input to bitwise e.g. 'ENT_IGNORE' becomes 4
+      if (OPTS[quote_style[i]] === 0) {
+        noquotes = true
+      } else if (OPTS[quote_style[i]]) {
+        optTemp = optTemp | OPTS[quote_style[i]]
+      }
+    }
+    quote_style = optTemp
+  }
+  if (quote_style & OPTS.ENT_HTML_QUOTE_SINGLE) {
+    string = string.replace(/'/g, '&#039;')
+  }
+  if (!noquotes) {
+    string = string.replace(/"/g, '&quot;')
+  }
+
+  return string
+}
+
+
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
@@ -31,9 +102,9 @@ angular.module('starter', ['ionic', 'ngMessages', '720kb.tooltips', 'bwcModule',
 })
 
 .controller('mainController', function($scope, $q, $ionicLoading, $ionicPopup, cscService, bwcService, CONFIG) {
-	$scope.show = function() {
+	$scope.show = function(template) {
 		$ionicLoading.show({
-			template: '<p>Retrieving wallet info...</p><ion-spinner></ion-spinner>',
+			template: template,
 			delay: 0
 		});
 	};
@@ -50,6 +121,7 @@ angular.module('starter', ['ionic', 'ngMessages', '720kb.tooltips', 'bwcModule',
 		
 //		var words1="bargain crowd rule giggle divert brave wool west refuse ski sustain neglect",
 //			words2="wife diesel actor broken clarify banana dignity craft exchange bar intact harbor";
+//			address="2MzENBSFsusyfDzUQ8xbd7fEcvBfRk6Rk32";
 
 		var walletClient1 = bwcService.getClient(),
 			walletClient2 = bwcService.getClient(),
@@ -58,7 +130,7 @@ angular.module('starter', ['ionic', 'ngMessages', '720kb.tooltips', 'bwcModule',
 		var deferred1 = $q.defer(),
 			deferred2 = $q.defer();
 
-		$scope.show(); // Shows the $ionicLoading spinner
+		$scope.show('<p>Retrieving wallet info...</p><ion-spinner></ion-spinner>'); // Shows the $ionicLoading spinner
 
 		setTimeout(function() {
 			$q.all([deferred1.promise, deferred2.promise]).then(function(result) { // Resolved
@@ -85,52 +157,168 @@ angular.module('starter', ['ionic', 'ngMessages', '720kb.tooltips', 'bwcModule',
 				var e1 = new Buffer(entropy1, 'base64');
 				var e2 = new Buffer(entropy2, 'base64');
 				var seed = Buffer.concat(sort([e1, e2]));
-				if (seed.length != 64) {
-					var popUp = $ionicPopup.show({
-						title: "Errore nella generazione dell'entropia",
-						subTitle: '(ERR_NOT512BIT_ENTROPY)',
-						scope: $scope,
-						buttons: [
-							{ text: 'Ok',
-							  type: 'button-positive'
-							},
-						]
-					});
-					
+				if (seed.length != 64) { 
+					alert("Errore nella generazione dell'entropia (ERR_NOT512BIT_ENTROPY)")
 					return;	
 				}
 				var xpriv = bwcService.getBitcore().HDPrivateKey.fromSeed(seed, CONFIG.NETWORK).toString();
 
 				walletClient3.seedFromExtendedPrivateKey(xpriv);
 
-				var popUp = $ionicPopup.show({
-					title: "Chiave recuperata:",
-					subTitle: xpriv,
-					scope: $scope,
-					buttons: [
-						{ text: 'Ok',
-						  type: 'button-positive'
-						},
-					]
-				});
+				if (dataForm.target.title) {
+			        $ionicPopup.confirm({
+			          title: 'Chiave recuperata',
+			          template: "Trasferire il portafoglio all'indirizzo " + dataForm.target.title + " ?"
+			        }).then(function(yes) {
 
+			        	if (yes) {
+			        		// Ok, confirmed!
+							var deferredFee = $q.defer(),
+								deferredAmount = $q.defer(),
+								deferredTxp = $q.defer(),
+								deferredSigned1 = $q.defer(),
+								deferredSigned2 = $q.defer();
+								deferredBroadcast = $q.defer();
 
-				// TODO
+							$scope.show('<p>Moving ...</p><ion-spinner></ion-spinner>'); // Shows the $ionicLoading spinner
+							
+							deferredFee.promise.then(function(feePerKB) {
+								// Now we do have the feePerKB, so let's retrive the max available balance
+								walletClient2.getBalance(function(err, balance) {
+									var feeToSendMaxSat = parseInt(((balance.totalBytesToSendMax * feePerKB) / 1000.).toFixed(0));
+								
+									if (balance.availableAmount > feeToSendMaxSat) {
+										if (balance.lockedAmount) { // Any pending transaction?
+											walletClient2.getTxProposals({}, function(err, proposals) {
+												if (err) {
+													deferredAmount.reject(err);
+												} else { // Let's abort them
+													
+													if (!proposals.length) throw("Internal error: lockedAmount > 0 with no pending txp");
+													
+													var deferred = [];
+													for (var i=0; i < proposals.length; i++) {
+														deferred.push($q.defer());
+													}
+													
+													$q.all(deferred.map(function(d) {
+														return d.promise;
+													})).then(function(result) {
+														deferredAmount.resolve(balance.availableAmount - feeToSendMaxSat);
+													}, function(err) {
+														deferredAmount.reject(err);
+													}); 											console.debug(proposals);
+													
+													proposals.forEach(function(txp, i) {
+														walletClient2.rejectTxProposal(txp, "Resetting wallet", function(err, obj) {
+															if (err) {
+																deferred[i].reject(err);
+															} else {						console.debug(obj);
+																deferred[i].resolve();
+															}
+														});
+													});
+												}
+											});
+										} else {
+											deferredAmount.resolve(balance.availableAmount - feeToSendMaxSat);
+										}
+									} else {
+										deferredAmount.reject("Not enough satoshis");
+									}
+								});
+								
+								$scope.feePerKB = feePerKB;
+							});
+							
+							deferredAmount.promise.then(function(availableMaxBalance) {
+								// We have our max available Balance. Let's move it
+								walletClient1.sendTxProposal({
+									toAddress: dataForm.target.title,
+									amount: availableMaxBalance,
+									message: '',
+									feePerKb: $scope.feePerKB,
+									excludeUnconfirmedUtxos:  false
+								}, function(err, txp) {
+									if (err) {
+										deferredTxp.reject(err);
+									} else {
+										deferredTxp.resolve(txp)
+									}
+								});
+							});
 
+							deferredTxp.promise.then(function(txp) {
+								// Transaction Proposal. Let's sign it.
+								walletClient1.signTxProposal(txp, function(err, signedTxp) {
+									if (err) {
+										deferredSigned1.reject(err);
+									} else {
+										deferredSigned1.resolve(signedTxp);
+									}
+								});
+							});
 
+							deferredSigned1.promise.then(function(signedTxp) {
+								// The transaction proposal is now signed once.
+								walletClient2.signTxProposal(signedTxp, function(err, signedTxp) {
+									if (err) {
+										deferredSigned2.reject(err);
+									} else {
+										deferredSigned2.resolve(signedTxp);
+									}
+								});
+							});
+
+							deferredSigned2.promise.then(function(signedTxp) {
+								// The transaction proposal is now signed twice. Broadcast!
+								walletClient2.broadcastTxProposal(signedTxp, function (err, btx, memo) {
+									if (err) {
+										deferredBroadcast.reject(err);
+									} else {
+										deferredBroadcast.resolve([btx, memo]);
+										alert("Transazione completata correttamente.\nTrasferiti " + btx.amount + " satoshi");
+									}
+								});
+							});
+							
+							$q.all([deferredFee.promise,
+									deferredAmount.promise,
+									deferredTxp.promise,
+									deferredSigned1.promise,
+									deferredSigned2.promise,
+									deferredBroadcast.promise
+							]).catch(function(err) {
+								alert("Errore nel trasferimento del portafoglio (" + err + ")");
+							}).finally(function() {
+								$scope.hide();
+							});
+
+							// Trigger the promise chain
+							walletClient1.getFeeLevels(walletClient1.credentials.network, function (err, levels) {
+								if (err) {
+									deferredFee.reject(err);
+								} else {
+									deferredFee.resolve(levels.filter(function(obj) {
+										return obj.level === "normal";
+									})[0].feePerKB);
+								}
+							});
+
+			        	} else {
+			        		alert("Operazione annullata!")
+			        	}
+			        });
+				} else { // No address supplied => No transfer requested
+					alert("Chiave recuperata: " + xpriv);
+				}
 
 			}, function(reason) { // Rejected
 				// At least one promise did not resolve
-				var popUp = $ionicPopup.show({
-					title: 'Errore nella creazione/recupero del wallet',
-					subTitle: 'Risposta del server: ' + reason,
-					scope: $scope,
-					buttons: [
-						{ text: 'Ok',
-						  type: 'button-positive'
-						},
-					]
-				});
+			    var alertPopup = $ionicPopup.alert({
+			      title: 'Errore nella creazione/recupero del wallet',
+			      template: 'Risposta del server: ' + htmlspecialchars(reason.toString())
+			    });
 			}).finally(function () {
 				$scope.hide();  // Hides the $ionicLoading spinner
 			});
@@ -142,7 +330,7 @@ angular.module('starter', ['ionic', 'ngMessages', '720kb.tooltips', 'bwcModule',
 						try {
 							walletClient1.seedFromMnemonic(dataForm.secret1.title, {network : CONFIG.NETWORK});
 							deferred1.resolve("wallet1 recreated");
-						} catch (e) {
+						} catch (e) { 
 							deferred1.reject(e);
 						}
 					} else {
@@ -192,7 +380,7 @@ angular.module('starter', ['ionic', 'ngMessages', '720kb.tooltips', 'bwcModule',
 	      function customValidator(ngModelValue) {
         
 	          // check if it's a twelve words phrase
-	          // if it is, set our custom `uppercaseValidator` to valid/true
+	          // if it is, set our custom `twelveWordsValidator` to valid/true
 	          // otherwise set it to non-valid/false
 	          if (/^([A-Za-z]+\s){11}[A-Za-z]+$/.test(ngModelValue)) {
 	              ctrl.$setValidity('twelveWordsValidator', true);
@@ -210,10 +398,10 @@ angular.module('starter', ['ionic', 'ngMessages', '720kb.tooltips', 'bwcModule',
 	      ctrl.$parsers.push(customValidator);
             
       }
-    };
+    }
 })
 
-.directive('target-address', function() {
+.directive('bcAddress', function() {
     return {
 
       // limit usage to argument only
@@ -227,17 +415,14 @@ angular.module('starter', ['ionic', 'ngMessages', '720kb.tooltips', 'bwcModule',
 		  
 	      function customValidator(ngModelValue) {
         
-	          // check if it's a twelve words phrase
-	          // if it is, set our custom `uppercaseValidator` to valid/true
+	          // check if it's a combination of letters and numbers (no white spaces)
+	          // if it is, set our custom `addressValidator` to valid/true
 	          // otherwise set it to non-valid/false
-			  
-			  // TODOOOOOOO
-			  
-/*	          if (/^([A-Za-z]+\s){11}[A-Za-z]+$/.test(ngModelValue)) {
-	              ctrl.$setValidity('twelveWordsValidator', true);
+	          if (/^[A-Za-z1-9]*$/.test(ngModelValue)) {
+	              ctrl.$setValidity('addressValidator', true);
 	          } else {
-	              ctrl.$setValidity('twelveWordsValidator', false);
-	          }*/
+	              ctrl.$setValidity('addressValidator', false);
+	          }
 
 	          // we need to return our ngModelValue, to be displayed to the user(value of the input)
 	          return ngModelValue;
@@ -249,5 +434,5 @@ angular.module('starter', ['ionic', 'ngMessages', '720kb.tooltips', 'bwcModule',
 	      ctrl.$parsers.push(customValidator);
             
       }
-    };
+    }
 });
