681490e0f3d344bd363fa878d7c42326dcafb8e9
Marco Polci <mpolci@gmail.com>
2016-04-21 16:13:31 +0200

    Add gulp task for angular-bitcore-wallet-client compilation

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,19 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var bower = require('bower');
+var browserify = require('gulp-browserify');
 var concat = require('gulp-concat');
 var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var sh = require('shelljs');
+var uglify = require('gulp-uglify');
 
 var paths = {
   sass: ['./scss/**/*.scss']
 };
 
-gulp.task('default', ['sass']);
+gulp.task('default', ['sass', 'libs']);
 
 gulp.task('sass', function(done) {
   gulp.src('./scss/ionic.app.scss')
@@ -24,6 +26,16 @@ gulp.task('sass', function(done) {
     .pipe(rename({ extname: '.min.css' }))
     .pipe(gulp.dest('./www/css/'))
     .on('end', done);
+});
+
+gulp.task('libs', function (done) {
+  gulp.src('./node_modules/angular-bitcore-wallet-client/index.js')
+    .pipe(rename("angular-bitcore-wallet-client.js"))
+    .pipe(browserify({}))
+    .pipe(gulp.dest('./www/lib/'))
+    .pipe(uglify())
+    .pipe(rename({ extname: '.min.js' }))
+    .pipe(gulp.dest('./www/lib/'));
 });
 
 gulp.task('watch', function() {
