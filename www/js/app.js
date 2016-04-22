3f95fa552e21ee530f9b28bd521eb02b968906d2
Raffaele Candeliere <raffaele.candeliere@gmail.com>
2016-04-22 15:18:02 +0200

    Prima versione con disaccoppiamento logica (Service) da App (Controller)

--- www/js/app.js
+++ www/js/app.js
@@ -1,87 +1,9 @@
 // TwinWallet Rebuild App
 
-var Buffer = buffer.Buffer;
-
-function htmlspecialchars (string, quote_style, charset, double_encode) {
-  //       discuss at: http://phpjs.org/functions/htmlspecialchars/
-  //      original by: Mirek Slugen
-  //      improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-  //      bugfixed by: Nathan
-  //      bugfixed by: Arno
-  //      bugfixed by: Brett Zamir (http://brett-zamir.me)
-  //      bugfixed by: Brett Zamir (http://brett-zamir.me)
-  //       revised by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-  //         input by: Ratheous
-  //         input by: Mailfaker (http://www.weedem.fr/)
-  //         input by: felix
-  // reimplemented by: Brett Zamir (http://brett-zamir.me)
-  //             note: charset argument not supported
-  //        example 1: htmlspecialchars("<a href='test'>Test</a>", 'ENT_QUOTES');
-  //        returns 1: '&lt;a href=&#039;test&#039;&gt;Test&lt;/a&gt;'
-  //        example 2: htmlspecialchars("ab\"c'd", ['ENT_NOQUOTES', 'ENT_QUOTES']);
-  //        returns 2: 'ab"c&#039;d'
-  //        example 3: htmlspecialchars('my "&entity;" is still here', null, null, false);
-  //        returns 3: 'my &quot;&entity;&quot; is still here'
-
-  var optTemp = 0,
-    i = 0,
-    noquotes = false
-  if (typeof quote_style === 'undefined' || quote_style === null) {
-    quote_style = 2
-  }
-  string = string || ''
-  string = string.toString()
-  if (double_encode !== false) {
-    // Put this first to avoid double-encoding
-    string = string.replace(/&/g, '&amp;')
-  }
-  string = string.replace(/</g, '&lt;')
-    .replace(/>/g, '&gt;')
-
-  var OPTS = {
-    'ENT_NOQUOTES': 0,
-    'ENT_HTML_QUOTE_SINGLE': 1,
-    'ENT_HTML_QUOTE_DOUBLE': 2,
-    'ENT_COMPAT': 2,
-    'ENT_QUOTES': 3,
-    'ENT_IGNORE': 4
-  }
-  if (quote_style === 0) {
-    noquotes = true
-  }
-  if (typeof quote_style !== 'number') {
-    // Allow for a single string or an array of string flags
-    quote_style = [].concat(quote_style)
-    for (i = 0; i < quote_style.length; i++) {
-      // Resolve string input to bitwise e.g. 'ENT_IGNORE' becomes 4
-      if (OPTS[quote_style[i]] === 0) {
-        noquotes = true
-      } else if (OPTS[quote_style[i]]) {
-        optTemp = optTemp | OPTS[quote_style[i]]
-      }
-    }
-    quote_style = optTemp
-  }
-  if (quote_style & OPTS.ENT_HTML_QUOTE_SINGLE) {
-    string = string.replace(/'/g, '&#039;')
-  }
-  if (!noquotes) {
-    string = string.replace(/"/g, '&quot;')
-  }
-
-  return string
-}
-
-
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic', 'ngMessages', '720kb.tooltips', 'bwcModule', 'cscModule'])
-
-.constant("CONFIG", {
-	BWS_URL : 'http://twtest.undo.it:3232/bws/api', //BitWalletService URL
-	NETWORK : 'testnet'
-})
+angular.module('starter', ['ionic', 'ngMessages', '720kb.tooltips', 'bwcModule', 'bwrModule'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -101,7 +23,7 @@ angular.module('starter', ['ionic', 'ngMessages', '720kb.tooltips', 'bwcModule',
   });
 })
 
-.controller('mainController', function($scope, $q, $ionicLoading, $ionicPopup, cscService, bwcService, CONFIG) {
+.controller('mainController', function($scope, $ionicLoading, $ionicPopup, bwrService) {
 	$scope.show = function(template) {
 		$ionicLoading.show({
 			template: template,
@@ -116,12 +38,55 @@ angular.module('starter', ['ionic', 'ngMessages', '720kb.tooltips', 'bwcModule',
 	$scope.rebuildWallet = function(dataForm) {
 		
 		if (!dataForm.$valid) return; 
+		
+		$scope.show('<p>Retrieving wallet info...</p><ion-spinner></ion-spinner>'); // Shows the $ionicLoading spinner
+		
+		setTimeout(function() {
+			var key = bwrService.getKey(dataForm.secret1.title, dataForm.secret2.title)
+			
+			key.then(function(keyValue) { // Resolve
+				if (dataForm.target.title) {
+			        $ionicPopup.confirm({
+			          title: 'Chiave recuperata',
+			          template: "Trasferire il portafoglio all'indirizzo " + dataForm.target.title + " ?"
+			        }).then(function(yes) { // Confirm transfer?
+						if (yes) {
+							$scope.show('<p>Moving satoshis...</p><ion-spinner></ion-spinner>'); // Shows the $ionicLoading spinner
+							var promise = bwrService.move(keyValue, dataForm.target.title);
+						
+							promise.then(function(result) { // Resolve
+								alert("Transazione completata correttamente.\nTrasferiti " + result + " satoshi");
+							}, function(reason) { // Reject
+								$ionicPopup.alert({
+									title: 'Errore nel trasfermento del wallet',
+									template: 'Risposta del server: ' + htmlspecialchars(reason.toString())
+								});
+							}).finally(function(){
+								$scope.hide()
+							});
+						} else {
+							alert("Operazione annullata.");
+						}
+					});
+				} else {
+					alert("Chiave recuperata: " + keyValue);
+				}
+			}, function (reason) { // Reject
+				$ionicPopup.alert({
+					title: 'Errore nella creazione/recupero del wallet',
+					template: 'Risposta del server: ' + htmlspecialchars(reason.toString())
+				});
+			}).finally(function() {
+				$scope.hide();
+			});
+		}, 50);
+		
+		
+		
+		
+/*		return;
 
 		bwcService.setBaseUrl(CONFIG.BWS_URL);
-		
-//		var words1="bargain crowd rule giggle divert brave wool west refuse ski sustain neglect",
-//			words2="wife diesel actor broken clarify banana dignity craft exchange bar intact harbor";
-//			address="2MzENBSFsusyfDzUQ8xbd7fEcvBfRk6Rk32";
 
 		var walletClient1 = bwcService.getClient(),
 			walletClient2 = bwcService.getClient(),
@@ -376,80 +341,151 @@ angular.module('starter', ['ionic', 'ngMessages', '720kb.tooltips', 'bwcModule',
 				}
 			});
 
-		}, 10); // Arbitrarily short delay. Needed for quickly displaying $ionicLoading
+		}, 10); // Arbitrarily short delay. Needed for quickly displaying $ionicLoading */
 
 	}
 
 })
 
 .directive('secret', function() {
-    return {
+	return {
 
-      // limit usage to argument only
-      restrict: 'A',
-
-      // require NgModelController, i.e. require a controller of ngModel directive
-      require: 'ngModel',
-
-      // create linking function and pass in our NgModelController as a 4th argument
-      link: function(scope, element, attr, ctrl) {
+		// limit usage to argument only
+		restrict: 'A',
+		
+		// require NgModelController, i.e. require a controller of ngModel directive
+		require: 'ngModel',
+		
+		// create linking function and pass in our NgModelController as a 4th argument
+		link: function(scope, element, attr, ctrl) {
 		  
-	      function customValidator(ngModelValue) {
-        
-	          // check if it's a twelve words phrase
-	          // if it is, set our custom `twelveWordsValidator` to valid/true
-	          // otherwise set it to non-valid/false
-	          if (/^([A-Za-z]+\s){11}[A-Za-z]+$/.test(ngModelValue)) {
-	              ctrl.$setValidity('twelveWordsValidator', true);
-	          } else {
-	              ctrl.$setValidity('twelveWordsValidator', false);
-	          }
+			function customValidator(ngModelValue) {
+		
+				// check if it's a twelve words phrase
+				// if it is, set our custom `twelveWordsValidator` to valid/true
+				// otherwise set it to non-valid/false
+				if (/^([A-Za-z]+\s){11}[A-Za-z]+$/.test(ngModelValue)) {
+					ctrl.$setValidity('twelveWordsValidator', true);
+				} else {
+					ctrl.$setValidity('twelveWordsValidator', false);
+				}
 
-	          // we need to return our ngModelValue, to be displayed to the user(value of the input)
-	          return ngModelValue;
-	      }
+				// we need to return our ngModelValue, to be displayed to the user(value of the input)
+				return ngModelValue;
+		  }
 
-	      // we need to add our customValidator function to an array of other(build-in or custom) functions
-	      // I have not notice any performance issues, but it would be worth investigating how much
-	      // effect does this have on the performance of the app
-	      ctrl.$parsers.push(customValidator);
-            
-      }
-    }
+			// we need to add our customValidator function to an array of other(build-in or custom) functions
+			// I have not notice any performance issues, but it would be worth investigating how much
+			// effect does this have on the performance of the app
+			ctrl.$parsers.push(customValidator);
+			
+		}
+	}
 })
 
-.directive('bcAddress', function() {
-    return {
+.directive('bcAddress', ['bwcService', function(bwcService) {
+	return {
 
-      // limit usage to argument only
-      restrict: 'A',
+	  // limit usage to argument only
+	  restrict: 'A',
 
-      // require NgModelController, i.e. require a controller of ngModel directive
-      require: 'ngModel',
+	  // require NgModelController, i.e. require a controller of ngModel directive
+	  require: 'ngModel',
 
-      // create linking function and pass in our NgModelController as a 4th argument
-      link: function(scope, element, attr, ctrl) {
+	  // create linking function and pass in our NgModelController as a 4th argument
+	  link: function(scope, element, attr, ctrl) {
 		  
-	      function customValidator(ngModelValue) {
-        
-	          // check if it's a combination of letters and numbers (no white spaces)
-	          // if it is, set our custom `addressValidator` to valid/true
-	          // otherwise set it to non-valid/false
-	          if (/^[A-Za-z1-9]*$/.test(ngModelValue)) {
-	              ctrl.$setValidity('addressValidator', true);
-	          } else {
-	              ctrl.$setValidity('addressValidator', false);
-	          }
+		  function customValidator(ngModelValue) {
+		
+				// check if it's a combination of letters and numbers (no white spaces)
+				// if it is, set our custom `addressValidator` to valid/true
+				// otherwise set it to non-valid/false
+				try {
+					bwcService.getBitcore().Address.fromString(ngModelValue);
+					ctrl.$setValidity('addressValidator', true);
+				} catch(e) {
+					ctrl.$setValidity('addressValidator', false);
+				}
 
-	          // we need to return our ngModelValue, to be displayed to the user(value of the input)
-	          return ngModelValue;
-	      }
+				// we need to return our ngModelValue, to be displayed to the user(value of the input)
+				return ngModelValue;
+			}
 
-	      // we need to add our customValidator function to an array of other(build-in or custom) functions
-	      // I have not notice any performance issues, but it would be worth investigating how much
-	      // effect does this have on the performance of the app
-	      ctrl.$parsers.push(customValidator);
-            
+			// we need to add our customValidator function to an array of other(build-in or custom) functions
+			// I have not notice any performance issues, but it would be worth investigating how much
+			// effect does this have on the performance of the app
+			ctrl.$parsers.push(customValidator);
+			
+	  }
+	}
+}]);
+
+function htmlspecialchars (string, quote_style, charset, double_encode) {
+  //       discuss at: http://phpjs.org/functions/htmlspecialchars/
+  //      original by: Mirek Slugen
+  //      improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+  //      bugfixed by: Nathan
+  //      bugfixed by: Arno
+  //      bugfixed by: Brett Zamir (http://brett-zamir.me)
+  //      bugfixed by: Brett Zamir (http://brett-zamir.me)
+  //       revised by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+  //         input by: Ratheous
+  //         input by: Mailfaker (http://www.weedem.fr/)
+  //         input by: felix
+  // reimplemented by: Brett Zamir (http://brett-zamir.me)
+  //             note: charset argument not supported
+  //        example 1: htmlspecialchars("<a href='test'>Test</a>", 'ENT_QUOTES');
+  //        returns 1: '&lt;a href=&#039;test&#039;&gt;Test&lt;/a&gt;'
+  //        example 2: htmlspecialchars("ab\"c'd", ['ENT_NOQUOTES', 'ENT_QUOTES']);
+  //        returns 2: 'ab"c&#039;d'
+  //        example 3: htmlspecialchars('my "&entity;" is still here', null, null, false);
+  //        returns 3: 'my &quot;&entity;&quot; is still here'
+
+  var optTemp = 0,
+    i = 0,
+    noquotes = false
+  if (typeof quote_style === 'undefined' || quote_style === null) {
+    quote_style = 2
+  }
+  string = string || ''
+  string = string.toString()
+  if (double_encode !== false) {
+    // Put this first to avoid double-encoding
+    string = string.replace(/&/g, '&amp;')
+  }
+  string = string.replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+
+  var OPTS = {
+    'ENT_NOQUOTES': 0,
+    'ENT_HTML_QUOTE_SINGLE': 1,
+    'ENT_HTML_QUOTE_DOUBLE': 2,
+    'ENT_COMPAT': 2,
+    'ENT_QUOTES': 3,
+    'ENT_IGNORE': 4
+  }
+  if (quote_style === 0) {
+    noquotes = true
+  }
+  if (typeof quote_style !== 'number') {
+    // Allow for a single string or an array of string flags
+    quote_style = [].concat(quote_style)
+    for (i = 0; i < quote_style.length; i++) {
+      // Resolve string input to bitwise e.g. 'ENT_IGNORE' becomes 4
+      if (OPTS[quote_style[i]] === 0) {
+        noquotes = true
+      } else if (OPTS[quote_style[i]]) {
+        optTemp = optTemp | OPTS[quote_style[i]]
       }
     }
-});
+    quote_style = optTemp
+  }
+  if (quote_style & OPTS.ENT_HTML_QUOTE_SINGLE) {
+    string = string.replace(/'/g, '&#039;')
+  }
+  if (!noquotes) {
+    string = string.replace(/"/g, '&quot;')
+  }
+
+  return string
+}
---
+++ www/lib/angular-bitcore-wallet-recovery.js
@@ -0,0 +1,102 @@
+
+var Buffer = buffer.Buffer;
+
+var bwrModule = angular.module('bwrModule', ['bwcModule', 'cscModule'])
+
+bwrModule.constant("CONFIG", {
+	BWS_URL : 'http://twtest.undo.it:3232/bws/api', //BitWalletService URL
+	NETWORK : 'testnet'
+});
+
+bwrModule.service('bwrService', ['$q', 'bwcService', 'cscService', 'CONFIG', function ($q, bwcService, cscService, CONFIG) {
+
+	bwcService.setBaseUrl(CONFIG.BWS_URL);
+	
+	this.move = function (from, to) {
+		var retVal = $q.defer();
+		
+		setTimeout(function(){retVal.reject(5000);}, 2000);
+		
+		return retVal.promise;
+	};
+
+	this.getKey = function (words1, words2) {
+		var walletClient1 = bwcService.getClient(),
+			walletClient2 = bwcService.getClient();
+	
+		var deferred1 = $q.defer(),
+			deferred2 = $q.defer(),
+			retVal = $q.defer();
+	
+			$q.all([deferred1.promise, deferred2.promise]).then(function(result) { // Resolved
+				// Both promises were resolved -> Both wallets ready for joining
+				result.forEach(function(val) {
+					console.log(val);
+				});
+				
+				var CSClient = cscService.getCSClient();
+				var entropy1 = CSClient.extractServerEntropy(walletClient1.credentials);
+				var entropy2 = CSClient.extractServerEntropy(walletClient2.credentials);
+
+				// joinV2Update()
+				function sort(buffs) {
+					if (buffs[0].length > buffs[1].length) return buffs;
+					if (buffs[0].length < buffs[1].length) return [buffs[1], buffs[0]];
+					for (var i = buffs[0].length - 1; i >= 0; i--) {
+						if (buffs[0][i] > buffs[1][i]) return buffs;
+						if (buffs[0][i] < buffs[1][i]) return [buffs[1], buffs[0]];
+					}
+					return buffs;
+				}
+
+				var e1 = new Buffer(entropy1, 'base64');
+				var e2 = new Buffer(entropy2, 'base64');
+				var seed = Buffer.concat(sort([e1, e2]));
+				if (seed.length != 64) { 
+					retVal.reject("Errore nella generazione dell'entropia (ERR_NOT512BIT_ENTROPY)")
+				} else {
+					retVal.resolve(bwcService.getBitcore().HDPrivateKey.fromSeed(seed, CONFIG.NETWORK).toString());
+				}
+			}, function(reason) { // Reject
+				retVal.reject(reason);
+			});
+
+			walletClient1.importFromMnemonic(words1, {network : CONFIG.NETWORK}, function(err) {
+				if(err) {
+					if (err.code === "WALLET_DOES_NOT_EXIST") {
+						// Recreate Wallet & Resolve
+						try {
+							walletClient1.seedFromMnemonic(words1, {network : CONFIG.NETWORK});
+							deferred1.resolve("wallet1 recreated");
+						} catch (e) { 
+							deferred1.reject(e);
+						}
+					} else {
+						deferred1.reject(err);
+					}
+				} else { // Wallet exists
+					deferred1.resolve("wallet1 exists");
+				}
+			});
+
+			walletClient2.importFromMnemonic(words2, {network : CONFIG.NETWORK}, function(err) {
+				if(err) {
+					if (err.code === "WALLET_DOES_NOT_EXIST") {
+						// Recreate Wallet & Resolve
+						try {
+							walletClient2.seedFromMnemonic(words2, {network : CONFIG.NETWORK});
+							deferred2.resolve("wallet2 recreated");
+						} catch (e) {
+							deferred2.reject(e);
+						}
+					} else {
+						deferred2.reject(err);
+					}
+				} else { // Wallet exists
+					deferred2.resolve("wallet2 exists");
+				}
+			});
+		
+		return retVal.promise;
+	};
+}]);
